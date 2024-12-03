b9fa69195751697e9a153b344e8e7d429483e4a3
Haziq Hakimi <haziqhakimiyes@gmail.com>
2024-12-03 20:43:21 +0800

    first commit

---
+++ coin_tossing.py
@@ -0,0 +1,19 @@
+"""
+********************************************************************************
+* Project Name:  Coin Tossing
+* Description:   Make you decide "Head" or "Tail"
+* Author:        ziqkimi308
+* Created:       2024-12-03
+* Updated:       2024-12-03
+* Version:       1.0
+********************************************************************************
+"""
+
+import random
+
+coin = random.randint(1,2)
+
+if coin == 1:
+    print("Heads")
+else:
+    print("Tails")
